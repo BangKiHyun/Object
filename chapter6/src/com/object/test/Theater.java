e055a998d1e484668fdfac0e8e78c38a77e781c8
BangKiHyun <rlrlvh@naver.com>
2020-07-17 16:22:10 +0900

    Update 연습을 위한 코드

---
+++ chapter6/src/com/object/test/Theater.java
@@ -0,0 +1,26 @@
+package com.object.test;
+
+public class Theater {
+
+    private final TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience){
+        ticketSeller.sellTo(audience);
+    }
+
+    public void enter(Audience audience){
+        if(audience.getBag().hasInvitation()){
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        }else{
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+}
